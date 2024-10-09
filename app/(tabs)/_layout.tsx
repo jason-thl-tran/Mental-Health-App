dbfc4b3b785c3c206fe6433d1be60222e2c4e8a6
jason-thl-tran <87417183+jason-thl-tran@users.noreply.github.com>
2024-10-09 00:07:09 -0500

    Updated splash page and logo
    uploaded splash page and logo. removed index.tsx and replaced with home.tsx (wip)

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout() {
       }}
     >
       <Tabs.Screen
-        name="index"
+        name="home"
         options={{
           title: "Home",
           tabBarIcon: ({ color, focused }) => (
